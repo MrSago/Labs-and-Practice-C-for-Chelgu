838a126cbfab97bd770e28ad6ffcfe2fc1db7792
Alexander <37180898+MrSago@users.noreply.github.com>
2022-01-10 16:08:44 +0300

    lab1 update

--- Lab_1/main.c
+++ Lab_1/main.c
@@ -1,141 +1,111 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define INPUT_FILE "input.txt"
 #define OUTPUT_FILE "output.txt"
 
-int main(int argc, char** argv, char** envp)
-{
-	FILE* file;
-	char str[11];
-	char bin[31];
-	int num = 0;
-	int sum = 0;
-	int i, p, tmp;
+int main(int argc, char** argv, char** envp) {
+    FILE* file;
+    char str[11];
+    char bin[31];
+    int num = 0;
+    int sum = 0;
+    int i, p, tmp;
 
 
-	if (fopen_s(&file, INPUT_FILE, "r") != 0 || file == (FILE*)NULL)
-	{
-		printf("Can't open file to read: %s\n", INPUT_FILE);
-		_getch();
-		return (EXIT_FAILURE);
-	}
+	file = fopen(INPUT_FILE, "r");
+    if (file == (FILE*)NULL) {
+        printf("Can't open file to read: %s\n", INPUT_FILE);
+        return 1;
+    }
 
-	for (i = 0; (str[i] = fgetc(file)) != EOF; ++i)
-	{
-		if (i + 1 > 10)
-		{
-			printf("Overflow type int!\n");
-			_getch();
-			return (EXIT_FAILURE);
-		}
+    for (i = 0; (str[i] = fgetc(file)) != EOF; ++i) {
+        if (i + 1 > 10) {
+            printf("Overflow type int!\n");
+            return 2;
+        }
 
-		if (str[i] < '0' || str[i] > '9')
-		{
-			printf("It's not a number!\n");
-			_getch();
-			return (EXIT_FAILURE);
-		}
-	}
-	str[i--] = 0;
-	fclose(file);
+        if (str[i] < '0' || str[i] > '9') {
+            printf("It's not a number!\n");
+            return 3;
+        }
+    }
+    str[i--] = 0;
+    fclose(file);
 
-	for (p = 1; i >= 0; --i, p *= 10)
-	{
-		if ((str[i] - '0') > INT_MAX / p)
-		{
-			printf("Overflow type int!\n");
-			_getch();
-			return (EXIT_FAILURE);
-		}
+    for (p = 1; i >= 0; --i, p *= 10) {
+        if ((str[i] - '0') > INT_MAX / p) {
+            printf("Overflow type int!\n");
+            return 2;
+        }
 
-		tmp = (str[i] - '0') * p;
+        tmp = (str[i] - '0') * p;
 
-		if (num <= INT_MAX - tmp)
-		{
-			num += tmp;
-		}
-		else
-		{
-			printf("Overflow type int!\n");
-			_getch();
-			return (EXIT_FAILURE);
-		}
-	}
+        if (num <= INT_MAX - tmp) {
+            num += tmp;
+        } else {
+            printf("Overflow type int!\n");
+            return 2;
+        }
+    }
 
-	for (tmp = num; tmp != 0; tmp /= 10)
-	{
-		sum += tmp % 10;
-	}
+    for (tmp = num; tmp != 0; tmp /= 10) {
+        sum += tmp % 10;
+    }
 
-	for (tmp = num, i = 0; tmp != 0; tmp /= 2)
-	{
-		bin[i++] = (char)(tmp % 2) + '0';
-	}
+    for (tmp = num, i = 0; tmp != 0; tmp /= 2) {
+        bin[i++] = (char)(tmp % 2) + '0';
+    }
 
-	printf("Number from file %s: %d\n", INPUT_FILE, num);
-	printf("Sum of digits: %d\n", sum);
-	printf("Binary: ");
-	for (tmp = 0, p = i; p > 0; --p)
-	{
-		if (tmp == 1)
-		{
-			if (p % 4 == 0)
-			{
-				putchar(' ');
-			}
-		}
-		else
-		{
-			if (p % 4 != 0)
-			{
-				for (tmp = 4 - (p % 4); tmp != 0; --tmp)
-				{
-					putchar('0');
-				}
-			}
+    printf("Number from file %s: %d\n", INPUT_FILE, num);
+    printf("Sum of digits: %d\n", sum);
+    printf("Binary: ");
+    for (tmp = 0, p = i; p > 0; --p) {
+        if (tmp == 1) {
+            if (p % 4 == 0) {
+                putchar(' ');
+            }
+        } else {
+            if (p % 4 != 0) {
+                for (tmp = 4 - (p % 4); tmp != 0; --tmp) {
+                    putchar('0');
+                }
+            }
 
-			tmp = 1;
-		}
+            tmp = 1;
+        }
 
-		putchar(bin[p - 1]);
-	}
+        putchar(bin[p - 1]);
+    }
 
-	if (fopen_s(&file, OUTPUT_FILE, "w") != 0 || file == (FILE*)NULL)
-	{
-		printf("\nCan't open file to write: %s\n", OUTPUT_FILE);
-		_getch();
-		return (EXIT_FAILURE);
-	}
+	file = fopen(OUTPUT_FILE, "w");
+    if (file == (FILE*)NULL) {
+        printf("\nCan't open file to write: %s\n", OUTPUT_FILE);
+        return 1;
+    }
 
-	for (tmp = 0; i > 0; --i)
-	{
-		if (tmp == 1)
-		{
-			if (i % 4 == 0)
-			{
-				fputc(' ', file);
-			}
-		}
-		else
-		{
-			if (i % 4 != 0)
-			{
-				for (tmp = 4 - (i % 4); tmp != 0; --tmp)
-				{
-					fputc('0', file);
-				}
-			}
+    for (tmp = 0; i > 0; --i) {
+        if (tmp == 1) {
+            if (i % 4 == 0) {
+                fputc(' ', file);
+            }
+        } else {
+            if (i % 4 != 0) {
+                for (tmp = 4 - (i % 4); tmp != 0; --tmp) {
+                    fputc('0', file);
+                }
+            }
 
-			tmp = 1;
-		}
+            tmp = 1;
+        }
 
-		fputc(bin[i - 1], file);
-	}
+        fputc(bin[i - 1], file);
+    }
 
-	fclose(file);
-	printf("\nResult has written to file: %s\n", OUTPUT_FILE);
+    fclose(file);
+    printf("\nResult has written to file: %s\n", OUTPUT_FILE);
 
-	_getch();
-	return (0);
+    return 0;
 }
+
