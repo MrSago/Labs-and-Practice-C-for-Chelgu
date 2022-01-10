1cfc232ef7cc5b239d769592dc6f35ee4649dc60
Alexander <37180898+MrSago@users.noreply.github.com>
2022-01-10 16:08:36 +0300

    ascii update

--- ASCII/main.c
+++ ASCII/main.c
@@ -1,41 +1,35 @@
+
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <conio.h>
-#pragma warning(disable:6031)
 
-int main(int argc, char** argv, char** envp)
-{
-	const char* FNAME = { "ASCII.txt" };
-	FILE* file;
-	int i;
+int main() {
+    const char* FNAME = "ASCII.txt";
+    FILE* file;
+    int i;
 
 
-	printf("Full ASCII table:\n");
-	
-	if (fopen_s(&file, FNAME, "w") == 0 && file != (FILE*)NULL)
-	{
-		fprintf_s(file, "Full ASCII table:\n");
+    printf("Full ASCII table:\n");
 
-		for (i = CHAR_MIN; i <= CHAR_MAX; ++i)
-		{
-			printf("Symbol with num:	%d = '%c'\n", i, (char)i);
-			fprintf_s(file, "Symbol with num:	%d = '%c'\n", i, (char)i);
-		}
+	file = fopen(FNAME, "w");
+    if (file != (FILE*)NULL) {
+        fprintf(file, "Full ASCII table:\n");
 
-		printf("Result has written to file: ASCII.txt\n");
-		fclose(file);
-	}
-	else
-	{
-		for (i = CHAR_MIN; i <= CHAR_MAX; ++i)
-		{
-			printf("Symbol with num : %d = '%c'\n", i, (char)i);
-		}
+        for (i = CHAR_MIN; i <= CHAR_MAX; ++i) {
+            printf("Symbol with num:	%d = '%c'\n", i, (char)i);
+            fprintf(file, "Symbol with num:	%d = '%c'\n", i, (char)i);
+        }
 
-		printf("Can't write result in file!\n");
-	}
+        printf("Result has written to file: ASCII.txt\n");
+        fclose(file);
+    } else {
+        for (i = CHAR_MIN; i <= CHAR_MAX; ++i) {
+            printf("Symbol with num : %d = '%c'\n", i, (char)i);
+        }
 
-	_getch();
-	return (0);
+        printf("Can't write result in file!\n");
+    }
+
+    return 0;
 }
+
