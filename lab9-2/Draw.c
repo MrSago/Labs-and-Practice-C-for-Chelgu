87baa595bc637faf7567c59ded2dc0fa45e7540c
Alexander <37180898+MrSago@users.noreply.github.com>
2022-01-20 18:42:28 +0300

    refact lab9-2

---
+++ lab9-2/Draw.c
@@ -0,0 +1,54 @@
+
+#include "main.h"
+
+void setconsolesize(int x, int y) {
+    HANDLE hCons;
+    COORD crd;
+    SMALL_RECT rct;
+    crd.X = (short)x + 1;
+    crd.Y = (short)y + 1;
+    rct.Left = 0;
+    rct.Top = 0;
+    rct.Right = x;
+    rct.Bottom = y;
+    hCons = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleScreenBufferSize(hCons, crd);
+    SetConsoleWindowInfo(hCons, 1, &rct);
+}
+
+void hidecursor() {
+    HANDLE hCons;
+    CONSOLE_CURSOR_INFO cci;
+    hCons = GetStdHandle(STD_OUTPUT_HANDLE);
+    cci.dwSize = 1;
+    cci.bVisible = 0;
+    SetConsoleCursorInfo(hCons, &cci);
+}
+
+void clrscr() {
+    HANDLE hCons;
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    COORD crd;
+    int len;
+    DWORD bRet;
+    short x, y;
+    char space = 0x20;
+    hCons = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleScreenBufferInfo(hCons, &csbi);
+    x = csbi.dwSize.X;
+    y = csbi.dwSize.Y;
+    len = (int)x * y;
+    crd.X = 0;
+    crd.Y = 0;
+    FillConsoleOutputCharacter(hCons, space, len, crd, &bRet);
+}
+
+void gotoxy(int column, int row) {
+    HANDLE hCons;
+    COORD crd;
+    hCons = GetStdHandle(STD_OUTPUT_HANDLE);
+    crd.X = (short)column;
+    crd.Y = (short)row;
+    SetConsoleCursorPosition(hCons, crd);
+}
+
