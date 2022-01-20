2c17d97baaeb5757acdaf39705619fa32a78d172
Alexander <37180898+MrSago@users.noreply.github.com>
2022-01-20 18:40:14 +0300

    refact lab7

---
+++ lab7/main.h
@@ -0,0 +1,22 @@
+
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include "locale.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+#define FILENAME_RESULT "Result.txt"
+#define FILENAME_REPORT "Report.txt"
+#define FILENAME_STRINGS "Strings.txt"
+
+void BruteForce(unsigned int result, char* string, char filename[]);
+void FullBruteForce(char filename[]);
+unsigned int HashRot13(char* str);
+char* ScanString();
+int MenuSelection();
+void StringBruteForce();
+
+#endif
+
